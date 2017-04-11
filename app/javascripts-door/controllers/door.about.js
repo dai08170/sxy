84ed708a609c5a6ffdd88b6c22836a0e59dddd20
lonelydawn <lonelydawn@sina.com>
2017-04-11 17:16:39 +0800

    1. 联系我们增加地图位置显示

--- app/javascripts-door/controllers/door.about.js
+++ app/javascripts-door/controllers/door.about.js
@@ -3,6 +3,27 @@
  */
 
 app.controller('aboutCtrl',['$scope', function($scope){
+    // 在高德地图上显示 位置
+    $(function(){
+        var map = new AMap.Map('mapContainer', {
+            resizeEnable: true,
+            zoom:15,
+            center: [117.1558500000, 34.2155720000]
+        });
+
+        // 工具bar 放大缩小 位置移动
+        map.plugin(["AMap.ToolBar"], function() {
+            map.addControl(new AMap.ToolBar());
+        });
+
+        // 添加标注
+        var marker = new AMap.Marker({
+            position : [117.1558500000, 34.2155720000],
+            title : "格局商学徐州分院",
+            map : map
+        });
+    });
+
     var data = global_config.about;
 
     $scope.college = data.college;
--- app/javascripts-door/controllers/door.xuzhou.js
+++ app/javascripts-door/controllers/door.xuzhou.js
@@ -3,5 +3,7 @@
  */
 
 app.controller('xuzhouCtrl',['$scope','$state',function($scope,$state){
+    var data = global_config.xuzhou;
 
+    $scope.president = data.president;
 }]);
