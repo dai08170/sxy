3074b4d5f02b773e46eb4347b15ebada3f9d10bb
lonelydawn <lonelydawn@sina.com>
2017-05-17 14:28:26 +0800

    添加远程服务器后台接口

--- routes/config.js
+++ routes/config.js
@@ -3,6 +3,7 @@
  */
 
 module.exports = {
-    url1: "http://127.0.0.1:8080",
+    // url1: "http://127.0.0.1:8080",
     url2: "http://127.0.0.1:80/index.php"
+    // url2: "http://118.89.224.209/index.php"
 };
